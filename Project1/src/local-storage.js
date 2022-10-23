5d51779a66cd4fbd1884f56310feba93ac1a3584
Miles Michell <milesmichell30@gmail.com>
2022-10-23 01:59:06 -0400

    save state near done

--- Project1/src/local-storage.js
+++ Project1/src/local-storage.js
@@ -1,6 +1,11 @@
 /* Checked by ESLint - https://eslint.org/demo */
 const defaultData = {
-    "haikus": []
+    "haikus": [],
+    // Saved search terms from app page
+    "savedSearch" : " ",
+    "numeralInput" : "1",
+    "uiState" : "Type a phrase and click the <b>Search</b> button to view Haikus",
+    "recentlySearchedHaikus" : []
   },
   storeName = "msm6982-p1-settings"; 
   
@@ -21,21 +26,39 @@ const defaultData = {
     localStorage.setItem(storeName, JSON.stringify(allValues));
   };
   
-  export const clearLocalStorage = () => writeLocalStorage(defaultData);
+
+
+
+  // Save the current state of the app page, only overriden when the user searches correctly 
+  export const saveAppState = (savedSearch, numeralInput, uiState, recentlySearchedHaikus) => 
+  {
+    let allValues = readLocalStorage();
+
+    allValues.savedSearch = savedSearch;
+    allValues.numeralInput = numeralInput;
+    allValues.recentlySearchedHaikus = recentlySearchedHaikus;
+    allValues.uiState = uiState;
+
+    writeLocalStorage(allValues);
+  }
   
+
   export const removeFavorite = (str) => {
     const allValues = readLocalStorage();
 
-    const haikuObj = {
-      "line1" : str.line1,
-      "line2" : str.line2,
-      "line3" : str.line3,
-      };
-
     allValues.haikus.pop();
-    //if (index > -1)  allValues.haikus.splice(index, 1); 
     
     writeLocalStorage(allValues);
+  }
+
+  // Check if a haikus is in the favorites
+  export const findHaikuInFav = (line1, line2, line3) =>
+  {
+    return readLocalStorage().haikus.includes({
+      "line1" : line1,
+      "line2" : line2,
+      "line3" : line3,
+      });
   }
 
   export const addFavorite = (str) => {
@@ -49,6 +72,17 @@ const defaultData = {
     allValues.haikus.push(haikuObj);
     writeLocalStorage(allValues);
   };
+
+  export const getSavedSearch = () => readLocalStorage().savedSearch;
+
+  export const getSavedNumeralInput = () => readLocalStorage.numeralInput;
+
+  export const getSavedUI = () => readLocalStorage().uiState;
+
+  export const getRecentlySearchedHaikus = () => readLocalStorage().recentlySearchedHaikus;
+
+  export const clearLocalStorage = () => writeLocalStorage(defaultData);
+
   
   export const getFavorites = () => readLocalStorage().haikus;
   
